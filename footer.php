53f4eb7d3b77aabc3690bf66540bc0febb317162
unknown <saurabh2003nalawade@gmail.com>
2023-12-26 16:56:45 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer id="footer" class="footer">
+
+  <div class="footer-content position-relative">
+    <div class="container">
+      <div class="row">
+        <h3>S R CONSALTANT</h3>
+        <div class="col-lg-4 col-md-6">
+          <div class="footer-info">
+            <h3><span><h6>Aurangabad</h6></span></h3>
+            <p>
+              Ramji", H. No. 5-3-30, <br>
+              Behind Gurudwara, New Usmanpura, Aurangabad, Maharashtra 431005 <br><br>
+              <strong>Phone:</strong>+91 70204 01089 <br>
+              <strong>Email:</strong> srconsultant@yahoo.com<br>
+            </p>
+            <div class="social-links d-flex mt-3">
+              <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+          <div class="footer-info">
+            <h3><span><h6>Jalana</h6></span></h3>
+            <p>
+              Shop No. 26, Krishna Arcade,<br>
+              Beside Prayag Hospital,Station Road, Old Jalna, Jalna, Pin- 431203<br><br>
+              <strong>Phone:</strong>+91 7499676015 <br>
+              <strong>Email:</strong> jalna@srconsultant.co.in<br>
+            </p>
+          </div>
+        </div><!-- End footer info column-->
+
+        <div class="col-lg-4 col-md-6 ml-auto">
+          <!-- Move this column to the right -->
+          <div class="footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Terms of service</a></li>
+              <li><a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-legal text-center position-relative">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>S R Consaltant</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </div>
+
+</footer>
